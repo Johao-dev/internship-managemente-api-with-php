fd7bfafdf87c2f9a261e02065615ad882e6e355f
johaoDev <johaobravoespinoza1501@gmail.com>
2025-10-31 17:16:17 -0500

    Lavada de cara al mapper para que ahora entienda la sintaxis de camelCase a snake_case.

--- src/core/Mapper.php
+++ src/core/Mapper.php
@@ -6,7 +6,6 @@ use ReflectionClass;
 use ReflectionProperty;
 
 final class Mapper {
-
     private static array $propertyCache = [];
 
     public static function mapToDto(string $dtoClass, object $source): ?object {
@@ -17,21 +16,31 @@ final class Mapper {
         if (!isset(self::$propertyCache[$dtoClass])) {
             $reflectionDto = new ReflectionClass($dtoClass);
             $dtoProperties = $reflectionDto->getProperties(ReflectionProperty::IS_PUBLIC);
-            
-            self::$propertyCache[$dtoClass] = array_flip(array_map(fn($prop) => $prop->getName(), $dtoProperties));
+            self::$propertyCache[$dtoClass] = array_flip(array_map(fn($p) => $p->getName(), $dtoProperties));
         }
 
-        $dtoPropertyNames = self::$propertyCache[$dtoClass];
-        $dtoInstance = new $dtoClass();
-        $sourceProperties = get_object_vars($source);
+        $dtoProps = self::$propertyCache[$dtoClass];
+        $dto = new $dtoClass();
+        $sourceProps = get_object_vars($source);
 
-        foreach ($sourceProperties as $key => $value) {
-            if (isset($dtoPropertyNames[$key])) {
-                $dtoInstance->{$key} = $value;
+        foreach ($sourceProps as $key => $value) {
+            if (isset($dtoProps[$key])) {
+                $dto->{$key} = $value;
+                continue;
+            }
+
+            $camelKey = self::snakeToCamel($key);
+            if (isset($dtoProps[$camelKey])) {
+                $dto->{$camelKey} = $value;
+                continue;
+            }
+
+            if (is_object($value) && isset($dtoProps[$key])) {
+                $dto->{$key} = $value;
             }
         }
 
-        return $dtoInstance;
+        return $dto;
     }
 
     public static function mapToDtoArray(string $dtoClass, array $sourceArray): array {
@@ -45,5 +54,9 @@ final class Mapper {
             }
         }
         return $result;
+    }
+
+    private static function snakeToCamel(string $str): string {
+        return lcfirst(str_replace('_', '', ucwords($str, '_')));
     }
 }
