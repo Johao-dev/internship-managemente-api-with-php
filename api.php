3d66dabf81209fabb77f3bc21f516f9a6f3bcae7
johaoDev <johaobravoespinoza1501@gmail.com>
2025-11-04 07:58:58 -0500

    permite peticiones PATCH

--- api.php
+++ api.php
@@ -15,7 +15,7 @@ use Dotenv\Dotenv;
 
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 try {
